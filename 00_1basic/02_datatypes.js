8c77b31e2d83341728c175606fe3f71022201e0a
Madhu700800 <149264500+Madhu700800@users.noreply.github.com>
2023-11-13 15:47:24 +0000

    datatypes

---
+++ 00_1basic/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all Js code as newer version
+
+//alert("hello")
+//alert( 3 + 3 )//w e are using node js ,not browser
+
+console.log(3 + 3)
+
+ console.log("Hitesh")
+ let name = "madhu"
+ let age = 18 
+ let isLoggedIn = false
+
+
+                   // This is Primitive Data types//
+ // number => 2 to pwer 53
+ //begint
+ // string => "madhu"
+ // boolen =>true/false
+ //null => standalone value
+ // undefined => 
+ // symbol => unique
+                //this is object//
+//object
+
+//console.log(typeof "");
+//console.log(typeof age);
+console.log(typeof null);(object)
+//console.log(typeof  undefined);
