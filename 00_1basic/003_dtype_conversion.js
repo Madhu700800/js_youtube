d8a60f120a19269d6ae3ff54f9f536ce93b44a6b
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2023-11-29 15:34:20 +0000

    conversion

---
+++ 00_1basic/003_dtype_conversion.js
@@ -0,0 +1,73 @@
+// let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber)); 
+//console.log(valueInNumber);
+
+//null// check
+//let score = null
+//  console.log(typeof(score));
+//let scoreInNumber = Number(score)
+//console.log(typeof(scoreInNumber));
+//console.log(scoreInNumber);
+
+
+//undefined check
+
+//let score = undefined
+//console.log(typeof score);
+//let anyInNumber = Number(score)
+//console.log(typeof anyInNumber);
+
+//console.log(anyInNumber);
+
+//****************check*boolean*************/
+//let score = true
+//console.log(typeof score);
+//let boolInNumber = Number(score)
+//console.log(typeof (boolInNumber));
+//console.log(boolInNumber)
+
+// let score = false
+// console.log(typeof score);
+// let boolInNumber = Number(score)
+// console.log(typeof (boolInNumber));
+// console.log(boolInNumber)
+
+//****************name***************** */
+
+// let score = "madhab"
+// console.log(typeof score);
+// let boolInNumber = Number(score)
+// console.log(typeof (boolInNumber));
+// console.log(boolInNumber)
+
+//"33" => 33
+//"33abc" =>  not a number(NaN)
+//true => 1;
+// false =>0;
+
+// let isLoggedIn = 1
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn)
+
+// let isLoggedIn = ""
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn)
+
+// let isLoggedIn = "Mahabala"
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn)
+
+
+ //*******************number*to string********* */
+ let someNumber = 33
+let stringToNum = String(someNumber);
+console.log(stringToNum);
+console.log(typeof(stringToNum));
+
+
+
