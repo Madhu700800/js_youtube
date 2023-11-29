543b01b43913626b05000b5a65ebaf81e474bdca
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2023-11-29 06:49:17 +0000

    container config done

---
+++ 00_1basic/001_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+ let accountEmail = "madhu@hool.com"
+ var  accountPassword = "1345" 
+ accountCity="Jaipur"
+  
+// accountId = 2// not alloed 
+ console.log(accountId);
+ let accountState;
+ accountEmail = "madhu@hool.com"
+ accountPassword = "1345" 
+ accountCity="Jaipur"
+ console.table([accountId , accountEmail,accountPassword,accountCity,accountState])
+  
