6d395fc796052ba3c462bb51094b6bd8f6fda68e
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2023-12-09 07:36:25 +0000

    string

---
+++ 00_1basic/06_strings.js
@@ -0,0 +1,44 @@
+// const name = "madhu"
+// const repoCount = 50
+
+//console.log(name + repoCount); //not use this syntax
+
+// console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+// const gameName = new String(`madhu`)
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('h'));
+
+
+//  ex-2****************************
+
+// const gamename1 = new String('madhu-mm')
+// const newString = gamename1.substring(0,4)
+//  console.log(newString);
+
+//  const anotherString = gamename1.slice(-7,4)
+//  console.log(anotherString);
+
+
+// const neStringOne = "    madhu    "
+// console.log(neStringOne);
+// console.log(neStringOne.trim());
+
+ //const url = "https://madhu.com/madhu%20priya"
+
+ //console.log(url.replace('%20','-'));
+
+//console.log(url.includes('madhu')); 
+
+//  ex-3****************************
+
+const gamename1 = new String('madhu-mm-com')
+console.log(gamename1.split('-'));
+
+
+
