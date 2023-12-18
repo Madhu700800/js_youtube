c195f566c34dc1c81d57a60f66c9f53ea7051288
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2023-12-18 18:07:11 +0000

    array1

---
+++ 00_1basic/09_array1.js
@@ -0,0 +1,38 @@
+const myArray = [0, 1, 2, 3, 4, 5]
+
+// console.log(myArray[1]);
+// console.log(myArray[3]);
+// console.log(myArray[0]);
+
+// ******************array method*****************
+const myHero = ["Shakti","sarupa","Purnima"]
+const number = [1, 2, 3, 4]
+// console.log(number);
+// myArray.push(9)
+// myArray.pop();
+// -----------------------------
+// myArray.unshift(8)
+// myArray.shift()
+// -----------------------------
+
+// console.log(myArray.includes(21));
+// console.log(myArray.indexOf(9));
+// console.log(myArray.indexOf(4));
+// console.log(myArray);
+// const newArray = myArray.join();
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// slice splice
+
+console.log("A" , myArray);
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B" , myArray);
+const myn2 = myArray.splice(1,3)
+console.log("C" , myArray);
+console.log(myn2);
+
+
+
