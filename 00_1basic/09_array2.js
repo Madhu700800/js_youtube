ce2344cd830797a377a8f073a4aae5404e012244
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2023-12-20 17:36:42 +0000

    array2

---
+++ 00_1basic/09_array2.js
@@ -0,0 +1,30 @@
+const my_Hero = ["Shiva","Krishna","Hanuman","Ganesh"]
+const Myrelation = ["Father","Friend","Brother1","Brother2",]
+
+// my_Hero.push(Myrelation)
+// console.log(my_Hero);
+
+// my_Hero.concat(Myrelation)
+// console.log(my_Hero);
+// ------------------1---------------------
+// const myres = my_Hero.concat(Myrelation)
+// console.log(myres);
+// ------------------2---------------------
+// const myres1 = [...my_Hero,...Myrelation ];
+// console.log(myres1);
+// ------------------3---------------------
+const anynumber = [1,2,3,[4,5],6,[9,8,[7,1]]]
+// console.log(anynumber.flat(Infinity));
+
+// const nyNumberres =  anynumber.flat(Infinity);
+// const nyNumberres =  anynumber.flat(2);
+// console.log(nyNumberres);
+// ------------------4---------------------
+// console.log(Array.isArray("Madhu"));
+// console.log(Array.from("Madhu"));
+// ------------------5---------------------
+// const score1 = 100
+// const score2 = 300
+// const score3 = 500
+// console.log(Array.of(score1,score2,score3));
+
