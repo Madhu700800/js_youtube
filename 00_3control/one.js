0fe520fd2406b14f9576e9b155470413ebe88d0c
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-05-21 12:51:14 +0000

    control

---
+++ 00_3control/one.js
@@ -0,0 +1,61 @@
+// if statemaent
+// if(condition){
+
+// }
+// if(true){
+//     execute
+// }
+// if(false){
+//     not execute
+// }
+
+// #1**
+// const isuser = true
+
+// if(isuser){
+//     console.log("code execute");
+// }
+// console.log("code not execute");
+
+// #2** compairision operator
+//  <, > ,<=,>=,==,===, != ,!== ,&&,||,
+// = assign operator
+// **************************************
+// if( 2 != 3 ){
+//     console.log("if true execute");
+// }else{
+//     console.log("if false not execute");
+// }
+// console.log("if default cut");
+// **************************************
+// if( 2 === "2" ){
+//     console.log("if true tw0 is execute");
+// }
+// **************************************
+// const temprature = 50;
+// if(temprature < 30){
+//     console.log("less than 50");
+// }else{
+//     console.log("greater than 50");
+// }
+// console.log("execute");
+// #3**
+// **************************************
+// const temprature = 41;
+// if(temprature === 40){
+//     console.log("less than 50");
+// }else{
+//     console.log("greater than 50");
+// }
+// console.log("execute");
+
+// **************************************
+let score = 200;
+if(score > 100){
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
+console.log(`user power: ${power}`);
+
+
+
