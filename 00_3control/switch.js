e8d01908daca9874c3aeeffaf4f81b7b85cbc0ac
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-05-22 04:52:26 +0000

    switch and truthy command

---
+++ 00_3control/switch.js
@@ -0,0 +1,58 @@
+// switch (key) {   // syntax
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let month = 3;
+// switch (month) {
+//     case 1: 
+//         console.log("jan");
+//         break;
+//         case 2: 
+//         console.log("feb");
+//         break;
+//         case 3:
+//         console.log("march");
+//         break;
+//         case 4: 
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default");
+//         break;
+// }
+// ***********************************************
+
+// let day = "tuesday";
+// switch (day) {
+//     case "monday": 
+//         console.log("monday");
+//         break;
+//         case "tuesday": 
+//         console.log("tuesday");
+//         break;
+//         case "wednesday":
+//         console.log("wedn");
+//         break;
+//         case "thursday": 
+//         console.log("thurs");
+//         break;
+//         case "friday": 
+//         console.log("fri");
+//         break
+//         case "saturaday": 
+//         console.log("satu");
+//         break;
+//         case "sunday": 
+//         console.log("sunday");
+//         break;
+
+//     default:
+//         console.log("default");
+//         break;
+// }
