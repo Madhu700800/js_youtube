8655fa3c4f38a837157f136532b27e456cf421d7
MADHU PRIYA ROUT <149264500+Madhu700800@users.noreply.github.com>
2024-05-25 06:16:17 +0000

    done

--- 00_4iterations_loop/nine.js
+++ 00_4iterations_loop/nine.js
@@ -1,6 +1,38 @@
 //reduce
 const myNum = [1,2,3]
  
-const myTotal = myNum.reduce(function (acc , currvl) {
-    return acc 
-})
+// const myTotal = myNum.reduce(function (acc , currvl) {
+// console.log(`acc : ${acc} and currvl : ${currvl}`);
+    // return acc + currvl
+// },0)
+// console.log(myTotal);
+
+// const myTotal = myNum.reduce((acc,curval) => {
+//  return acc + curval
+
+// },0)
+// console.log(myTotal);
+// const  myTotal  = myNum.reduce((acc,curval) => acc + curval,0)
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName:"Js Course",
+        price:2999
+    },
+    {
+        itemName:"phython Course",
+        price:999
+    },
+    {
+        itemName:"mob dev Course",
+        price:5999
+    },
+    {
+        itemName:"Data Science Course",
+        price:12999
+    },
+]
+
+const shopping = shoppingCart.reduce((acc , items)=>  items.price + acc,0)
+console.log(shopping);
